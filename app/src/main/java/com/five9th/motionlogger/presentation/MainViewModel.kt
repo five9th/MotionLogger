99b8094721ff68f428d83d154f621dc8bc64be95
five9th <yegorbalamutin1@gmail.com>
2025-12-13 19:11:20 +0300

    timer and sample counter added to viewModel

--- app/src/main/java/com/five9th/motionlogger/presentation/MainViewModel.kt
+++ app/src/main/java/com/five9th/motionlogger/presentation/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.five9th.motionlogger.presentation
 
 import android.app.Application
+import android.os.SystemClock
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
@@ -16,23 +17,34 @@ import com.five9th.motionlogger.domain.usecases.SaveSamplesUseCase
 import com.five9th.motionlogger.domain.usecases.StartCollectUseCase
 import com.five9th.motionlogger.domain.usecases.StopCollectUseCase
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-// TODO: collect in foreground service
+data class CollectionStats(
+    val elapsedMillis: Long,
+    val samplesCount: Int
+)
+
+// (2) TODO: collect in foreground service
 // TODO: load samples to current_session.csv every few minutes
-// TODO: display timer (elapsed time and collected samples
+// TODO: display list of recorded sessions
+// TODO: add session id
 class MainViewModel(app: Application) : AndroidViewModel(app) {
     private val tag = "MainViewModel"
 
+    // ---- repos ----
     private val sensorsRepo = SensorsRepoImpl(app) // use DI (TODO)
     private val filesRepo = FilesRepoImpl(app) // use DI
 
+    // ---- Use Cases ----
     private val getSensorsInfoUseCase = GetSensorsInfoUseCase(sensorsRepo)
 
     private val observeSensorsUseCase = ObserveSensorsUseCase(sensorsRepo)
@@ -41,9 +53,21 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
 
     private val saveSamplesUseCase = SaveSamplesUseCase(filesRepo)
 
+    // ---- LiveData's and Flow's ----
     private val _sensorsInfoLD = MutableLiveData<SensorsInfo>()
-    val sensorsInfoLD: LiveData<SensorsInfo>
-        get() = _sensorsInfoLD
+    val sensorsInfoLD: LiveData<SensorsInfo> = _sensorsInfoLD
+
+    private val sensorDataSF: StateFlow<SensorSample?> = observeSensorsUseCase()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+
+    val isCollectingSF: StateFlow<Boolean> = sensorsRepo.isCollecting
+
+    private val _collectionStatsFS = MutableStateFlow(
+        CollectionStats(0L, 0)
+    )
+    val collectionStatsFS: StateFlow<CollectionStats> = _collectionStatsFS
+
+
 
     private val collectedSamples = mutableListOf<SensorSample>()
 
@@ -52,20 +76,19 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
 
     private var collectingJob: Job? = null
 
+    private var timerJob: Job? = null
+    private var startTimerTime: Long = 0L
+
     fun getSensorsInfo() {
         _sensorsInfoLD.value = getSensorsInfoUseCase()
     }
-
-    val sensorDataSF: StateFlow<SensorSample?> = observeSensorsUseCase()
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
-
-    val isCollectingSF: StateFlow<Boolean> = sensorsRepo.isCollecting
 
     fun startCollect() {
         if (isCollectingSF.value) return
 
         startCollectUseCase()  // tell repo to collect sensor data
         startCollectJob()  // saving samples from the flow to the list
+        startTimerJob()
     }
 
     private fun startCollectJob() {
@@ -83,11 +106,35 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
         }
     }
 
+    private fun startTimerJob() {
+        if (timerJob != null) return
+
+        startTimerTime = SystemClock.elapsedRealtime()
+
+        timerJob = viewModelScope.launch {
+            while (isActive) {
+                updateValuesByTimer()
+                delay(200) // update UI 5x per second (smooth, cheap)
+            }
+        }
+    }
+
+    private fun updateValuesByTimer() {
+        // elapsed time
+        val elapsed = SystemClock.elapsedRealtime() - startTimerTime
+
+        // samples count
+        val count = collectedSamples.size
+
+        _collectionStatsFS.value = CollectionStats(elapsed, count)
+    }
+
     fun stopCollect() {
         if (!isCollectingSF.value) return
 
         stopCollectUseCase()
         stopCollectJob()
+        stopTimerJob()
 
         stopTimestamp = System.currentTimeMillis()
 
@@ -97,6 +144,14 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     private fun stopCollectJob() {
         collectingJob?.cancel()
         collectingJob = null
+    }
+
+    private fun stopTimerJob() {
+        timerJob?.cancel()
+        timerJob = null
+
+        // reset values
+        _collectionStatsFS.value = CollectionStats(0L, 0)
     }
 
     private fun saveToCsv() {
