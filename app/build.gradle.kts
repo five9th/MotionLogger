5d38a775d5572990d464e674caa4768569b5f511
five9th <yegorbalamutin1@gmail.com>
2026-02-10 20:46:33 +0300

    set app version to 0.0.2-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.five9th.motionlogger"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.1-alpha"
+        versionCode = 2
+        versionName = "0.0.2-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
