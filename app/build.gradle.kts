e27a04fc6667546b75fa6e8ffba90149e1df3677
five9th <yegorbalamutin1@gmail.com>
2026-06-05 20:44:21 +0300

    set app version to '0.1.0-beta'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 2
-        versionName = "0.0.2-alpha"
+        versionName = "0.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
